1c34d2131782849e84306eec869bf6bfea041a6b
josaintus <josaintus@gmail.com>
2024-03-30 19:07:57 -0400

    First commit deploy app

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,13 +5,13 @@ function Navigation() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/home">
+        <Link className="navbar-brand" to="/">
           DevAccount Tech Solutions
         </Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/home">
+              <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
