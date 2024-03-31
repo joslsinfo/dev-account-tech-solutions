2762780db874532258563fa7baf1bf98b562d23b
josaintus <josaintus@gmail.com>
2024-03-30 22:50:12 -0400

    Update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,46 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 function Navigation() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link className="navbar-brand" to="/">
           DevAccount Tech Solutions
         </Link>
-        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
+        <div className="collapse navbar-collapse">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={toggleMenu}>
+              <Link className="nav-link" to="/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/development" onClick={toggleMenu}>
+              <Link className="nav-link" to="/development">
                 Development
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/accounting" onClick={toggleMenu}>
+              <Link className="nav-link" to="/accounting">
                 Accounting
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about" onClick={toggleMenu}>
+              <Link className="nav-link" to="/about">
                 About
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact" onClick={toggleMenu}>
+              <Link className="nav-link" to="/contact">
                 Contact
               </Link>
             </li>
