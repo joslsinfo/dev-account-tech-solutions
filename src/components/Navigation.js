c347c01d8308ab472092021189c871109a2fb1c5
josaintus <josaintus@gmail.com>
2024-03-30 22:59:57 -0400

    Update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,37 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link className="navbar-brand" to="/">
           DevAccount Tech Solutions
         </Link>
-        <div className="collapse navbar-collapse">
+        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-link" to="/" onClick={toggleMenu}>
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/development">
+              <Link className="nav-link" to="/development" onClick={toggleMenu}>
                 Development
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/accounting">
+              <Link className="nav-link" to="/accounting" onClick={toggleMenu}>
                 Accounting
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link className="nav-link" to="/about" onClick={toggleMenu}>
                 About
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
+              <Link className="nav-link" to="/contact" onClick={toggleMenu}>
                 Contact
               </Link>
             </li>
