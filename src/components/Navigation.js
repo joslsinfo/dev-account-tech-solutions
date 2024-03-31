1875680d802ac91fda1bcc6c86dcc75dc3c35c6b
josaintus <josaintus@gmail.com>
2024-03-30 21:49:28 -0400

    Update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,37 +1,95 @@
-import React from "react";
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// function Navigation() {
+//   return (
+//     <>
+//       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//         <Link className="navbar-brand" to="/">
+//           DevAccount Tech Solutions
+//         </Link>
+//         <div className="collapse navbar-collapse">
+//           <ul className="navbar-nav ms-auto">
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/">
+//                 Home
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/development">
+//                 Development
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/accounting">
+//                 Accounting
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/about">
+//                 About
+//               </Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/contact">
+//                 Contact
+//               </Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//       <h2 className="text-center">
+//         "Where Technology Meets Finance Expertise"
+//       </h2>
+//     </>
+//   );
+// }
+
+// export default Navigation;
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link className="navbar-brand" to="/">
           DevAccount Tech Solutions
         </Link>
-        <div className="collapse navbar-collapse">
+        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-link" to="/" onClick={toggleMenu}>
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/development">
+              <Link className="nav-link" to="/development" onClick={toggleMenu}>
                 Development
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/accounting">
+              <Link className="nav-link" to="/accounting" onClick={toggleMenu}>
                 Accounting
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link className="nav-link" to="/about" onClick={toggleMenu}>
                 About
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
+              <Link className="nav-link" to="/contact" onClick={toggleMenu}>
                 Contact
               </Link>
             </li>
